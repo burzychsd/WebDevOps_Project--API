8a88ad9c7be635c27089380b300258c64814a699
Burzychs <burzychs@gmail.com>
2018-10-25 16:20:24 +0200

    add gravatar

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express 		= require('express');
 const dotenv 		= require('dotenv/config');
 const mongoose		= require('mongoose');
 const bodyParser 	= require('body-parser');
+const gravatar		= require('gravatar');
 
 const users = require('./routes/users');
 
